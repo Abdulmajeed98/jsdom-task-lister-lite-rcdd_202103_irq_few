17fdc0d2a437bd3497d81e45f04e9a5bd7864080
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-13 01:56:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const inputText = document.getElementById('new-task-description');
-const form = document.getElementById('create-task-form');
-const listDiv = document.getElementById('list');
-const taskUL = document.getElementById('tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputText = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const listDiv = document.getElementById('list');
+  const taskUL = document.getElementById('tasks')
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     let inputTextValue = inputText.value;
